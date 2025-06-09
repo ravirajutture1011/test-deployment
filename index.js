3f8f8d518cb3230a229bc802196cfbd60bdb2063
ravirajutture1011 <rajutture1026@gmail.com>
2025-06-09 23:17:25 +0530

    vercel url added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://test-frontend-mf7y.vercel.app/",
     credentials:true,
 }));
 
